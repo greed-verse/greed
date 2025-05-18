e90dd8bea00dbf6b41c67f244597a1fce8bca951
Rishi-S20 <rishisuryavanshi14@gmail.com>
2025-05-18 16:50:37 -0500

    updates

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,7 @@
-import { useRouter } from "expo-router";
+import { Redirect, useRouter } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
 export default function Index() {
   const router = useRouter();
-  return (
-    <TouchableOpacity
-      style={styles.button}
-      onPress={() => router.push("/(unauth)/login")}
-    >
-      <Text style={styles.buttonText}>Go to Home</Text>
-    </TouchableOpacity>
-  );
+  return <Redirect href="/(unauth)/login" />;
 }
-
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: "#007AFF",
-    paddingVertical: 12,
-    paddingHorizontal: 24,
-    borderRadius: 8,
-    alignSelf: "center",
-    marginTop: 50,
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "600",
-  },
-});
